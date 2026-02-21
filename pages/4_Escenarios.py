2c669704b2d62aaa8eb3ccee9d02121e3ae2eb28
alexaalmadap <A01785664@tec.mx>
2026-02-20 20:45:51 -0600

    Create 4_Escenarios.py

---
+++ pages/4_Escenarios.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Escenarios")
+st.write("Sección en desarrollo (Semanas posteriores).")
+
+st.markdown("""
+Aquí irá:
+- Escenarios predefinidos
+- Sensibilidad a costos
+- Comparación de resultados
+""")
