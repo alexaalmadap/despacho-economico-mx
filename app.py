82d07893fc7d264e4a977815cfeec3e004735da4
alexaalmadap <A01785664@tec.mx>
2026-02-20 20:36:09 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Despacho Económico MX", layout="wide")
+
+st.title("Despacho Económico MX ⚡")
+st.write("Semana 1: estructura inicial del simulador.")
+
+st.info("En esta versión solo se muestra el esqueleto de la aplicación. Las funciones se implementarán en las siguientes semanas.")
