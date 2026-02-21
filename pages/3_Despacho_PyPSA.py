59f944e4b7012894d0598ff06de8dd72ebf0ee2c
alexaalmadap <A01785664@tec.mx>
2026-02-20 20:44:39 -0600

    Create 3_Despacho_PyPSA.py

---
+++ pages/3_Despacho_PyPSA.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Despacho Económico (PyPSA)")
+st.write("Sección en desarrollo (Semana 3).")
+
+st.markdown("""
+Aquí irá:
+- Construcción del modelo en PyPSA
+- Optimización del despacho
+- Resultados y visualizaciones
+""")
