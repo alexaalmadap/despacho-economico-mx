8425d52506f58cf7f47141dbf13e4c94fbfb0727
alexaalmadap <A01785664@tec.mx>
2026-02-20 20:39:04 -0600

    Create 2_Capacidades.py

---
+++ pages/2_Capacidades.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Capacidades de Generación")
+
+st.write("Sección en desarrollo (Semana 2).")
+
+st.markdown("""
+Aquí irá:
+- Carga de capacidades por sistema (SIN, BCA, BCS)
+- Tecnologías disponibles
+- Parámetros técnicos básicos
+""")
